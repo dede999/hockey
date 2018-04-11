0c9584b27f1799d3fa9870ec3894ffb0587a61eb
dede999 <andreluizsa00@gmail.com>
2018-04-11 09:50:49 -0300

    Refactoring DB (Part 1) -- Lot of work to be done

--- h_leagues/models.py
+++ h_leagues/models.py
@@ -9,6 +9,13 @@ LEAGUES = (
     ('WHL', 'Western Hockey League'),
 )
 
+PO_MARKS = (
+    ('x - ', 'Clinched Playoff Spot'),
+    ('y - ', 'Division Champion'),
+    ('z - ', 'Conference Champion'),
+    ('', '')
+)
+
 RESULTADOS = (
     ('', ''),
     ('f', 'FINAL'),
@@ -16,14 +23,39 @@ RESULTADOS = (
     ('so', 'FINAL-SO'),
 )
 
+class League(models.Model):
+    name= models.CharField(max_length=5, primary_key=True,choices=LEAGUES)
+    rs_games = models.IntegerField(default=1)
+    season = models.CharField(default='')
+    color = models.CharField(default='')
+    bg_color = models.CharField(default='')
+
+class Conference(models.Model):
+    name = models.CharField(max_length=30, primary_key=True)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    non_division = models.IntegerField(default=0)
+
+class Division(models.Model):
+    name = models.CharField(max_length=30, primary_key=True)
+    po_tops = models.IntegerField(default=1)
+
 class Team(models.Model):
     city = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
-    abr = models.CharField(max_length=5)
-    league = models.CharField(max_length=5, choices=LEAGUES)
-    conference = models.CharField(max_length=30, default='')
+    abr = models.CharField(max_length=5, primary_key=True)
+    # league = models.CharField(max_length=5, choices=LEAGUES)
+    # conference = models.CharField(max_length=30, default='')
     division = models.CharField(max_length=30, default='')
+    holder = models.BooleanField(default=False) # title holder
+
+    def __str__(self):
+        return "%s %s" % (self.city, self.name)
+
+class RS_clubs(models.Model):
+    eq = models.ForeignKey(Team, on_delete=models.CASCADE)
     seed = models.IntegerField(default=1)
+    p_seed = models.IntegerField(default=1)
+    po_marks = models.CharField(default='', choices=PO_MARKS) # title holder
     pts = models.IntegerField(default=0)
     wins = models.IntegerField(default=0)
     loss = models.IntegerField(default=0)
@@ -33,9 +65,11 @@ class Team(models.Model):
     ga = models.IntegerField(default=0)
     diff = models.IntegerField(default=0)
     streak = models.CharField(max_length=3, default='W 0')
+    games = models.ManyToManyField("self", through='Match', through_fields=('home', 'away'))
 
-    def __str__(self):
-        return "%s %s" % (self.city, self.name)
+    def __init__(self, team):
+        self.eq = team
+        self.save()
 
     def __lt__(self, other):
         if self.pts != other.pts:
@@ -44,8 +78,8 @@ class Team(models.Model):
             return self.diff > other.diff
         elif self.gf != other.gf:
             return self.gf > other.gf
-        elif self.city != other.city:
-            return  self.city < other.city
+        elif self.eq.city != other.eq.city:
+            return  self.eq.city < other.eq.city
 
     def seq(self, result):
         r = (self.streak).split(" ")
@@ -68,7 +102,7 @@ class Team(models.Model):
             return '%05.3f' % (self.pts/(jogos*2))
 
 
-class POseries(models.Model):
+class Series(models.Model):
     high = models.CharField(max_length=100, default='')
     h_msg = models.CharField(max_length=100, default='')
     h_wins = models.IntegerField(default=0)
@@ -81,17 +115,18 @@ class POseries(models.Model):
 
 
 class Match(models.Model):
-    away = models.CharField(max_length=50)
+    away = models.ForeignKey(RS_clubs, on_delete=models.CASCADE)
     a_score = models.IntegerField(default=0)
-    home = models.CharField(max_length=50)
+    home = models.ForeignKey(RS_clubs, on_delete=models.CASCADE)
     h_score = models.IntegerField(default=0)
-    league = models.CharField(max_length=5, choices=LEAGUES)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
     resultado = models.CharField(max_length=2, choices=RESULTADOS, default='')
     winner = models.CharField(max_length=50, default='')
 
+    # def __init__(self):
+
     def __lt__(self, other):
         return self.pk > other.pk
-
 
 # Create your models here.
 # from h_leagues.models import *
